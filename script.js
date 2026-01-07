50a503115d2bdeb3e6a69780330592aa6d33da00
Ravi shankar <shankarravi8005@gmail.com>
2026-01-07 11:51:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function generatePassword() {
+    const length = document.getElementById("length").value;
+    const uppercase = document.getElementById("uppercase").checked;
+    const lowercase = document.getElementById("lowercase").checked;
+    const numbers = document.getElementById("numbers").checked;
+    const symbols = document.getElementById("symbols").checked;
+
+    let characters = "";
+    let password = "";
+
+    if (uppercase) characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if (lowercase) characters += "abcdefghijklmnopqrstuvwxyz";
+    if (numbers) characters += "0123456789";
+    if (symbols) characters += "!@#$%^&*()_+[]{}|;:,.<>?";
+
+    if (characters === "") {
+        alert("Please select at least one option!");
+        return;
+    }
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        password += characters[randomIndex];
+    }
+
+    document.getElementById("password").value = password;
+}
